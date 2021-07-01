1d7e5bf796c89c55061168857095d7661eaf9ce8
JoyrenceMatthewAgas <63637289+JoyrenceMatthewAgas@users.noreply.github.com>
2021-07-01 20:48:57 +0800

    Memory functions

--- Library/memory.h
+++ Library/memory.h
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 
-void *get_memory(size_t size) {
+static void *get_memory(size_t size) {
     return malloc(size);
 }
 
-void free_memory(void *ptr) {
+static void free_memory(void *ptr) {
     return (free(ptr));
 }
