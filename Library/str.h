0d50a2d568ff0c058bda088c3f4dd984dac66148
JoyrenceMatthewAgas <63637289+JoyrenceMatthewAgas@users.noreply.github.com>
2021-07-01 08:13:59 +0800

    String functions

---
+++ Library/str.h
@@ -0,0 +1,51 @@
+#include <string.h>
+#include <stdlib.h>
+
+char *add_str(const char *s1, const char *s2) {
+    size_t s1_len = strlen(s1);
+    size_t s2_len = strlen(s2);
+    char *ret = malloc(s1_len + s2_len + 1);
+    memcpy(ret, s1, s1_len);
+
+    // copy the null terminator too
+    memcpy(ret + s1_len, s2, s2_len + 1);
+    return ret;
+}
+
+int compare_str(char *str, const char *compare) {
+    return (strcmp(str, compare));
+}
+
+char *copy_str(char *dest, const char *src) {
+    char *result = dest;
+    while (*src != '\0')
+    {
+        *dest = *src;
+        src++;
+        dest++;
+    }
+    *dest = '\0';
+    return result;
+}
+
+int get_length(char *str) {
+    return strlen(str);
+}
+
+char *lowercase(char *text) {
+    for (char *p = text; *p; ++p) {
+        if (isalpha((unsigned char) *p)) {
+            *p = tolower((unsigned char) *p);
+        }
+    }
+    return text;
+}
+
+char *uppercase(char *text) {
+    for (char *p = text; *p; ++p) {
+        if (isalpha((unsigned char) *p)) {
+            *p = toupper((unsigned char) *p);
+        }
+    }
+    return text;
+}
