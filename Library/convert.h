69f3635a535f27aee7c389b03a7f1db9e5273e6d
JoyrenceMatthewAgas <63637289+JoyrenceMatthewAgas@users.noreply.github.com>
2021-07-01 20:46:06 +0800

    Conversion functions

--- Library/convert.h
+++ Library/convert.h
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 #include <limits.h>
 
-char get_char(char *str, int pv) {
+static char get_char(char *str, int pv) {
     return str[pv];
 }
 
-char make_char(int val) {
+static char make_char(int val) {
     return (char)val;
 }
 
-double make_double(int num) {
+static double make_double(int num) {
     return (double)num;
 }
 
-float make_float(int num) {
+static float make_float(int num) {
     return (float)num;
 }
 
-int make_int(void *var) {
+static int make_int(void *var) {
     return atoi(var);
 }
 
-const char *make_str_base(char *buf, size_t len, int var) {
+const static char *make_str_base(char *buf, size_t len, int var) {
     static char loc_buf[sizeof(int) * CHAR_BIT];
     if (!buf) {
         buf = loc_buf;
@@ -34,6 +34,6 @@ const char *make_str_base(char *buf, size_t len, int var) {
     return buf;
 }
 
-const char *make_str(int var) {
+const static char *make_str(int var) {
     return make_str_base(NULL, 0, var);
 }
