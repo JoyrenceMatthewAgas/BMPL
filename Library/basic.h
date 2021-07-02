dfd8487ad652911ed0796c08bfbb99fc3db90109
JoyrenceMatthewAgas <63637289+JoyrenceMatthewAgas@users.noreply.github.com>
2021-07-02 09:01:51 +0800

    Input/output functions

--- Library/basic.h
+++ Library/basic.h
@@ -14,9 +14,19 @@
                         int:    "%d \n"  \
                 ), (X));
 
+/*
 int take(const char *prompt, char *result, size_t siz) {
     char fmt[32];
     printf("%s", prompt);
     sprintf(fmt, "%%%zus", siz - 1);
     return scanf(fmt, result);
+}
+*/
+
+char *take(const char *text) {
+    char result[256];
+    printf("%s", text);
+    *result = '\0';  // return an empty string on input error
+    scanf("%s", result);
+    return strdup(result);
 }
