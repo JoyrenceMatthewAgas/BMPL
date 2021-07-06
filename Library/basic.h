1eb76f258b3d6322c34915699bb404c9c6b7c4d4
JoyrenceMatthewAgas <63637289+JoyrenceMatthewAgas@users.noreply.github.com>
2021-07-06 17:56:45 +0800

    Update basic.h

--- Library/basic.h
+++ Library/basic.h
@@ -1,40 +1,7 @@
-#include <stdio.h>
+#ifndef BASIC_H
+#define BASIC_H
 
-#define say(X) printf(_Generic((X),   \
-                        double: "%f", \
-                        float:  "%f", \
-                        char *: "%s", \
-                        int:    "%d"  \
-                ), (X));
+char *take_str(const char *prompt, char *result, size_t size);
+void take_int(const char *prompt, int result);
 
-#define sayLn(X) printf(_Generic((X),   \
-                        double: "%f\n", \
-                        float:  "%f\n", \
-                        char *: "%s\n", \
-                        int:    "%d\n"  \
-                ), (X));
-
-/*
-int take(const char *prompt, char *result, size_t siz) {
-    char fmt[32];
-    printf("%s", prompt);
-    sprintf(fmt, "%%%zus", siz - 1);
-    return scanf(fmt, result);
-}
-*/
-
-char *take_str(const char *prompt, char *result, size_t size) {
-    char spec[32];
-    if (size == 0)
-        return NULL;
-    snprintf(spec, sizeof spec, "%%%zus", size - 1);
-    printf("%s", text);
-    *result = '\0';
-    scanf(spec, result);
-    return result;
-}
-
-void take_int(const char *prompt, int result) { // Needs debugging
-    say(prompt);
-    scanf("%i", result);
-}
+#endif
