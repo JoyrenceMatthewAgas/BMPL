46e24e87c17362ba5650aad43a6b49c23acf6a63
JoyrenceMatthewAgas <63637289+JoyrenceMatthewAgas@users.noreply.github.com>
2021-07-06 21:43:10 +0800

    Update mathematics.h

--- Library/mathematics.h
+++ Library/mathematics.h
@@ -1,4 +1,4 @@
-#include <math.h>
+#include "all.h"
 
 static int cube_root(int num) {
     return cbrt(num);
@@ -13,25 +13,9 @@ static double log_num(long long int num) {
 }
 
 static double power(double x, double y) {
-    int mult = x;
-    for (int i = 1; i < y; i++) {
-        mult *= x;
-    }
-    return mult;
+    return pow(x, y);
 }
 
 static int square_root(int num) {
-    int mult;
-    int result = 0;
-    for (int i = 1; i <= num; i++) {
-        mult = i * i;
-        if (mult > num) {
-            result = i - 1;
-            break;
-        } else if (mult == num) {
-            result = i;
-            break;
-        }
-    }
-    return result;
+    return sqrt(num);
 }
