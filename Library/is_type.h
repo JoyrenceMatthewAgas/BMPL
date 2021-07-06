bd527ce463d621bdd22b498f673d9b0df5b2effb
JoyrenceMatthewAgas <63637289+JoyrenceMatthewAgas@users.noreply.github.com>
2021-07-06 21:42:31 +0800

    Update is_type.h

--- Library/is_type.h
+++ Library/is_type.h
@@ -1,4 +1,4 @@
-#include <ctype.h>
+#include "all.h"
 
 static int is_num(char a) {
     return isdigit(c);
@@ -8,11 +8,11 @@ static int is_letter(char a) {
     return isalpha(a);
 }
 
-static int is_lowercase(char a) {
+static int is_lower(char a) {
     return islower(a);
 }
 
-static int is_uppercase(char a) {
+static int is_upper(char a) {
     return isupper(a);
 }
 
