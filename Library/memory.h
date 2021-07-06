3fa39cae16da96fd90bc1a6a465839ed87195590
JoyrenceMatthewAgas <63637289+JoyrenceMatthewAgas@users.noreply.github.com>
2021-07-06 21:43:55 +0800

    Update memory.h

--- Library/memory.h
+++ Library/memory.h
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 
-static void *get_memory(size_t size) {
-    return malloc(size);
+static void get_memory(size_t size) {
+    malloc(size);
 }
 
 static void free_memory(void *ptr) {
-    return (free(ptr));
+    free(ptr);
 }
